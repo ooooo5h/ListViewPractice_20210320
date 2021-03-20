44a0c76c00cd90abf3b740311c75b8bf23904c6c
전은형 <ooooo5h91@gmail.com>
2021-03-20 23:41:14 +0900

    화면코드에서 => 학생목록변수 + 실제 학생 데이터 주입

--- app/src/main/java/com/neppplus/listviewpractice_20210320/MainActivity.kt
+++ app/src/main/java/com/neppplus/listviewpractice_20210320/MainActivity.kt
@@ -2,10 +2,26 @@ package com.neppplus.listviewpractice_20210320
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.listviewpractice_20210320.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+//    학생 목록을 담아둘 ArrayList 변수 추가
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        학생 목록을 실제로 추가
+        mStudentList.add(Student("조경진", 1988))
+        mStudentList.add(Student("홍준호", 1995))
+        mStudentList.add(Student("김민철", 1990))
+        mStudentList.add(Student("김종진", 1991))
+        mStudentList.add(Student("장혜진", 1993))
+
+
+
+
     }
 }
